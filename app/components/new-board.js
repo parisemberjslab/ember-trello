c707faf0c15e6ca8ac0f1f397b532afa48fc5d88
Fred ST <frederic.soumare@gmail.com>
2017-10-15 15:06:50 +0200

    first commit

---
+++ app/components/new-board.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  actions: {
+    create() {
+      console.log("creating board");
+    }
+  },
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('board', {path: ':board_id'});
 });
 
 export default Router;
